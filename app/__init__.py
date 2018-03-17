3d4af896135fc3f5f44cb101ec29dc606b4d102d
KIMARI NELSON <kimari.nelson@mymona.uwi.edu>
2018-03-17 22:08:42 +0000

    Heroku setup

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ SECRET_KEY = 'Sup3r$3cretkey'
 app = Flask(__name__)
 app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg', 'gif'])
 app.config['SECRET_KEY'] = "kb161dfd61dgdg6ddfdd"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project1:password123@localhost/project1"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://capelcwvgbouzn:83904471a49b9745c1f67dae685630e6318ea452423a610d0e737b86ef448be6@ec2-54-163-246-193.compute-1.amazonaws.com:5432/d5cae0ljph75ar"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 app.config['UPLOAD_FOLDER'] = "./app/static/uploads"
 
